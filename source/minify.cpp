39f837ab99c10ba5fb74ba24c8aa19182705ff05
LotfyMabrouk <85258381+LotfyMabrouk@users.noreply.github.com>
2021-12-15 17:01:00 +0200

    Add files via upload

---
+++ source/minify.cpp
@@ -0,0 +1,28 @@
+#include"minify.h"
+
+void minify(string in,string out){
+	ifstream indata(in);
+	ofstream outdata(out);
+	string line;
+	while (getline(indata,line)){
+        for(int i =0;i<line.size()*2;i++){
+			if(line[line.size()-1]==' '){
+				line.erase(line.size()-1,1);
+			}
+			else if(line[line.size()-1]=='\t'){
+				line.erase(line.size()-1,1);
+			}
+			else if(line[0]=='\t'){
+				line.erase(0,1);
+			}
+			else if(line[0]==' '){
+				line.erase(0,1);
+			}
+			else{
+				break;
+			}
+        }
+        outdata<<line;
+	}
+	indata.close();
+}

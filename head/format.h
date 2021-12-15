09a651302f2e87f0b295cd013d1c041126266914
Ahmed-ali-abd-elsalam <85034642+Ahmed-ali-abd-elsalam@users.noreply.github.com>
2021-12-15 16:14:13 +0200

    Add files via upload

---
+++ head/format.h
@@ -0,0 +1,12 @@
+#ifndef FORMAT_H
+#define FORMAT_H
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<stack>
+using namespace std;
+
+void writeToOutput(string in, string out);
+void format(string in, string out);
+#endif

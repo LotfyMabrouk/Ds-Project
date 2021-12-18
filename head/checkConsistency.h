122cd98c8245d184a7316f6c5af56bd225ac2760
Ahmed-Ashraf-El-Sayed <85251349+Ahmed-Ashraf-El-Sayed@users.noreply.github.com>
2021-12-18 21:00:48 +0200

    pushing header for checkConsistency

---
+++ head/checkConsistency.h
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+bool ArePair(char opening, char closing);     // Function to check whether two characters are opening and closing of same type. 
+bool AreTagsBalanced(string exp);            //Function to check if tags are balanced.

ec06c4a4898f2d3bb30edf8d1b6a0d7c490d9591
Mohammad_Zaher_Aweira <85291326+Mohammad274@users.noreply.github.com>
2021-12-23 21:02:34 +0200

    Add files via upload

---
+++ head/XML2JSON.h
@@ -0,0 +1,14 @@
+#ifndef XML2JSON_H
+#define XML2JSON_H
+#include<iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+void format(string in,string out);
+void XML2JSON(string inputFile, string outputFile , string tempFile );
+string SetOfSpaces(int SpacesNum);
+#endif // XML2JSON_H

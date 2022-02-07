ecf765da5e80bd7502920794fd4ced02c03a87f6
Ahmed-ali-abd-elsalam <85034642+Ahmed-ali-abd-elsalam@users.noreply.github.com>
2022-02-08 01:42:34 +0200

    graph headers

---
+++ head/Graph.h
@@ -0,0 +1,9 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include<iostream>
+#include<bits/stdc++.h>
+#include<fstream>
+using namespace std;
+void dotParse(vector<string> vn , vector <string> vi , vector<vector <string>> vf , string fileName);
+void vectorextract(string in,string out);
+#endif
